6b8af8297634e071da36e88fbcb12a928be5ab8a
bayuyuhartono <bayuyuhartono@gmail.com>
2022-04-03 22:16:05 +0700

    tes

--- app/Controllers/Reservasi.php
+++ app/Controllers/Reservasi.php
@@ -13,7 +13,7 @@ class Reservasi extends BaseController
         //         return redirect()->to(base_url());
         //     }
         // }
-        echo json_encode($this->request->getGet('dari'));
+        echo json_encode($this->request);
         return;
         $reqData['berangkat'] = isset($bodyRaw['dari']) ? $bodyRaw['dari'] : '';
         $reqData['tujuan'] = isset($bodyRaw['tujuan']) ? $bodyRaw['tujuan'] : '';
