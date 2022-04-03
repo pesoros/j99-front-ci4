9ac7bc87567741520fb9869dda88b4397c2452a1
bayuyuhartono <bayuyuhartono@gmail.com>
2022-04-03 22:00:36 +0700

    tes

--- app/Controllers/Reservasi.php
+++ app/Controllers/Reservasi.php
@@ -6,7 +6,7 @@ class Reservasi extends BaseController
 {
     public function index()
     {
-        $bodyRaw = $this->request;
+        $bodyRaw = $this->request->getVar('dari');
         // if (isset($bodyRaw) <= 0) {
         //     $bodyRaw = session('dataToSave');
         //     if (isset($bodyRaw)) {
