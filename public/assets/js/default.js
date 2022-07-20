936b078415d4db45e7797c27846d91a593fef079
bayuyuhartono <bayuyuhartono@gmail.com>
2022-07-20 09:30:03 +0700

    cop

--- public/assets/js/default.js
+++ public/assets/js/default.js
@@ -275,3 +275,14 @@ function copyToClipboard(textToCopy) {
     console.log(textToCopy)
 }
 
+function copyToClipboard(copyText) {
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+    
+    /* Alert the copied text */
+    console.log("Copied the text: ", copyText.value);
+  }
