ff406f5998b4ac3f8ea94b152b074cb650ed799f
bayuyuhartono <bayuyuhartono@gmail.com>
2022-07-22 14:50:28 +0700

    copy

--- public/assets/js/default.js
+++ public/assets/js/default.js
@@ -266,13 +266,10 @@ function redirectBut(targetLink) {
 }
 
 function copyToClipboard(copyText) {
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
   
     /* Copy the text inside the text field */
-    navigator.clipboard.writeText(copyText.value);
+    navigator.clipboard.writeText(copyText);
     
     /* Alert the copied text */
-    console.log("Copied the text: ", copyText.value);
+    console.log("Copied the text: ", copyText);
   }
