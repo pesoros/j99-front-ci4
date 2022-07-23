5a7e5404cc27070cb3af3533013ad8da82645700
bayuyuhartono <bayuyuhartono@gmail.com>
2022-07-23 16:56:41 +0700

    dep

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -52,11 +52,10 @@ class Home extends BaseController
         curl_setopt($curl, CURLOPT_POST, true);                                                             
         curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));                                    
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);                                                   
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));   
         $result = curl_exec($curl);                                                                       
         curl_close($curl);                            ;                                                   
-        // $result = json_decode($result,TRUE);                                                                       
-        return $curl;                                                                      
+        $result = json_decode($result,TRUE);                                                                       
+        return $result;                                                                      
     }                
 
     public function httpGetXform($url)
