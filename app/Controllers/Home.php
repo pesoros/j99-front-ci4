06661b473911d58259a9bcd641fb96a2cd989ca6
bayuyuhartono <bayuyuhartono@gmail.com>
2022-07-23 16:24:53 +0700

    dep

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -18,7 +18,7 @@ class Home extends BaseController
         $reqData['keyword'] = '';
         $dataKelas = $this->httpPostXform(getenv('API_ENDPOINT')."datakelas",$reqData);
 
-        $reqData;
+        echo json_encode($reqData);
         return;
 
         $ldata['dataKota'] = $dataKota; 
