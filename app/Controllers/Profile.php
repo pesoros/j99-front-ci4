51a20c9243851c187bd8792d4a0e5404e3b58228
bayuyuhartono <bayuyuhartono@gmail.com>
2022-07-24 00:00:30 +0700

    a

--- app/Controllers/Profile.php
+++ app/Controllers/Profile.php
@@ -71,7 +71,8 @@ class Profile extends BaseController
         curl_setopt($curl, CURLOPT_POST, true);                                                             
         curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));                                    
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);                                                   
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);     
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);     
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
         $result = curl_exec($curl);                                                                       
         curl_close($curl);                            ;                                                   
