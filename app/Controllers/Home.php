df64200359a2e645dffe5fbbc0ce769afba84ed1
bayuyuhartono <bayuyuhartono@gmail.com>
2022-07-23 17:06:47 +0700

    dep

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -13,7 +13,7 @@ class Home extends BaseController
         session()->set('nowat', '');
         session()->set('roundTrip', '');
         $reqData['keyword'] = '';
-        $dataKota = $this->httpGetXform(getenv('API_ENDPOINT'));
+        $dataKota = $this->httpPostXform(getenv('API_ENDPOINT')."datakota",$reqData);
 
         $reqData['keyword'] = '';
         $dataKelas = $this->httpPostXform(getenv('API_ENDPOINT')."datakelas",$reqData);
@@ -53,6 +53,8 @@ class Home extends BaseController
         curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));                                    
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);                                                   
         curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));   
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);     
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
         $result = curl_exec($curl);                                                                       
         curl_close($curl);                            ;                                                   
         $result = json_decode($result,TRUE);                                                                       
